dc5d6633ce12e901ae5696b0be3da9c3fa5d319f
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-11 16:50:15 +0200

    feat calcolo prezzo base

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,32 @@
 // Pulsante dom
 const calculate = document.getElementById('calculate')
+
+// creo il click
+calculate.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const km = document.getElementById('km').value
+  const age = document.getElementById('age').value
+
+  // calcolo prezzo base
+  const baseprice = 0.21 * km;
+  let finalprice
+
+  if(age < 18){
+      finalprice = baseprice *0.8;
+  }
+  else if(age > 65){
+       finalprice = baseprice *0.6; 
+
+  } 
+  else{
+      finalprice = baseprice
+  }
+
+   results.innerText = `Km percorsi: ${km}, età del passeggero: ${age}, prezzo finale: ${finalprice}`
+})
+
+
+
+
+
